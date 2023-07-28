69280f871cf91ad869deb5ca644951b2b355373d
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-07-27 23:47:00 -0400

    Update promiseBasics.js

--- promiseBasics.js
+++ promiseBasics.js
@@ -18,3 +18,40 @@ count
 Promise kept
 More multiple functions...
 */
+
+
+/* Simple program with a promise. */
+const statement = false;
+
+let evalStatement = new Promise(function (resolve, reject) {
+    try {
+        if (statement) {  
+            resolve("The statement is true.");
+        } else {
+            reject("The statement is false.")
+        };
+    } catch {
+        ("ERROR");
+    }
+});
+
+console.log(evalStatement);
+
+/*
+Promise { <rejected> 'The statement is false.' }
+node:internal/process/promises:288
+            triggerUncaughtException(err, true );
+            ^
+
+[UnhandledPromiseRejection: This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). The promise rejected with the reason "The statement is false.".] {
+  code: 'ERR_UNHANDLED_REJECTION'
+}
+node:internal/process/promises:288
+            triggerUncaughtException(err, true /* fromPromise */);
+            ^
+
+[UnhandledPromiseRejection: This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). The promise rejected with the reason "The statement is false.".] {
+  code: 'ERR_UNHANDLED_REJECTION'
+}
+
+*/
